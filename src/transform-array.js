0c689d3a91d95f2efdc7f476f4ba0e92548f4bff
Alexander Leonov <spawnleon@gmail.com>
2021-02-24 19:45:16 +0300

    feat: tasks have been finished

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
-const CustomError = require("../extensions/custom-error");
+const ruleMap = {
+  '--double-next': 1,
+  '--double-prev': 2,
+  '--discard-next': 3,
+  '--discard-prev': 4,
+};
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+
+  const result = [];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    switch (ruleMap[arr[i]]) {
+      case 1:
+        result.push(arr[i + 1]);
+        break;
+      case 2:
+        result.push(result[result.length - 1]);
+        break;
+      case 3:
+        result.push(undefined);
+        i += 1;
+        break;
+      case 4:
+        result.pop();
+        break;
+      default:
+        result.push(arr[i]);
+    }
+  }
+  return result.filter(function(el) {
+    return el !== undefined;
+  });
 };
